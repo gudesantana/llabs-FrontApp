4731508396b6da3dc47ec60066fc255b4fe961cf
Windson Santana <wdavinsin@gmail.com>
2024-10-23 01:51:13 -0300

    change api key

--- src/App.js
+++ src/App.js
@@ -3,11 +3,13 @@ import axios from 'axios';
 
 function App() {
     const [message, setMessage] = useState('');
+    const [dados, setDados] = useState([]);
 
     useEffect(() => {
         axios.get('http://appback.good.tec.br:3000/atualizar-previsao')
             .then(response => {
-                setMessage(response.data);
+                setMessage('Dados salvos com sucesso!');
+                setDados(response.data);
             })
             .catch(error => {
                 setMessage(`Erro: ${error.message}`);
@@ -18,6 +20,18 @@ function App() {
         <div className="App">
             <h1>Atualização de Previsão do Tempo</h1>
             <p>{message}</p>
+            {dados.length > 0 && (
+                <div>
+                    <h2>Dados Gravados:</h2>
+                    <ul>
+                        {dados.map((dado, index) => (
+                            <li key={index}>
+                                País: {dado.country}, Data: {dado.date}, Descrição: {dado.text}
+                            </li>
+                        ))}
+                    </ul>
+                </div>
+            )}
         </div>
     );
 }
